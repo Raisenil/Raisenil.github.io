8e6458780497079834f550bbb97f51020a64e98c
Raise nil <raisulislamniloy47@gmail.com>
2023-06-11 23:48:38 +0600

    animation color fix

--- javascript/script.js
+++ javascript/script.js
@@ -40,4 +40,9 @@ window.onscroll = () => {
     // remove toggle icon and navbar when click navbar links (scroll)
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
+
+    // animation footer on scroll
+    let footer=document.querySelector('footer');
+
+    footer.clearList.toggle('show-animate', this.innerHeight + this.scrollY>=document.scrollingElement.scrollHeight);
 }
