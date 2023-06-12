25c13bfa1da3373c5a65a48259b56b7345a13be7
Raise nil <raisulislamniloy47@gmail.com>
2023-06-13 00:07:09 +0600

    Design done

--- javascript/script.js
+++ javascript/script.js
@@ -69,7 +69,7 @@ window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
         let windowHeight = window.innerHeight;
-        let offset = sec.offsetTop - (windowHeight * 0.50); // Updated scroll threshold to 80%
+        let offset = sec.offsetTop - (windowHeight * 0.50); // Updated scroll threshold to 50%
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
@@ -78,8 +78,10 @@ window.onscroll = () => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
+            // active sections for animation on scroll
             sec.classList.add('show-animate');
         } else {
+            // animation that repeats on scroll
             sec.classList.remove('show-animate');
         }
     });
@@ -90,9 +92,11 @@ window.onscroll = () => {
     let menuIcon = document.querySelector('#menu-icon');
     let navbar = document.querySelector('.navbar');
 
+    // remove toggle icon and navbar when click navbar links (scroll)
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 
+    // animation footer on scroll
     let footer = document.querySelector('footer');
     footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 };
