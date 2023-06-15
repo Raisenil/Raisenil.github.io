627ecafee15cef8b8e64b7142104640feff2ea9d
Raise nil <raisulislamniloy47@gmail.com>
2023-06-16 01:11:12 +0600

    Eail intrigated

--- javascript/email.js
+++ javascript/email.js
@@ -1,25 +1,38 @@
-function sendMail(){
+function sendMail() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var number = document.getElementById("number").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+
+    // Check if any required field is empty
+    if (!name || !email || !number || !subject || !message) {
+        alert("Please fill in all the required fields.");
+        return; // Stop execution if any field is empty
+    }
+
     var params = {
-        name:document.getElementById("name").value,
-        email:document.getElementById("email").value,
-        number:document.getElementById("number").value,
-        subject:document.getElementById("subject").value,
-        message:document.getElementById("message").value,
+        name: name,
+        email: email,
+        number: number,
+        subject: subject,
+        message: message
     };
 
-    const serviceID="service_iymp626";
-    const templateID="template_3viahrd";
-    
+    const serviceID = "service_iymp626";
+    const templateID = "template_3viahrd";
+
     emailjs
-        .send(serviceID,templateID,params)
+        .send(serviceID, templateID, params)
         .then((res) => {
-            document.getElementById("name").value="";
-            document.getElementById("email").value="";
-            document.getElementById("number").value="";
-            document.getElementById("subject").value="";
-            document.getElementById("message").value="";
+            // Clear input fields after successful submission
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("number").value = "";
+            document.getElementById("subject").value = "";
+            document.getElementById("message").value = "";
             console.log(res);
-            alert("your message sent successfully");
+            alert("Your message was sent successfully.");
         })
-        .catch((err)=>console.log(err));
+        .catch((err) => console.log(err));
 }
